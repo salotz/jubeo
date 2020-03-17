49ef525e8a1f469a566e15e7ce17a4408e7a3854
Samuel Lotz <samuel.lotz@salotz.info>
2020-03-17 00:28:41 -0400

    update cookiecutter module

--- modules/cookiecutter.py
+++ modules/cookiecutter.py
@@ -1,6 +1,35 @@
 from invoke import task
 
+#@task(pre=[clean])
 @task
-def test(cx):
-    raise NotImplementedError
+def test_render(cx, default=True, context_file=None):
+
+    cx.run("mkdir -p tests/_test_builds")
+
+    if default:
+        cx.run("cookiecutter -f --no-input -o tests/_test_builds/ .")
+
+    # read from a JSON file
+    elif context_file is not None:
+        assert osp.exists(context_file), f"context file {context_file} doesn't exist"
+
+        cx.run(f"cookiecutter -f --no-input -o tests/_test_builds/ . {context}")
+
+    # otherwise do interactively
+    else:
+        cx.run("cookiecutter -f -o tests/_test_builds/ .")
+
+
+@task(pre=[test_render])
+def test_rendered_repo(cx, default=True, context=None):
+
+    default_name = "default_repo_name"
+    target_dir = f'tests/_test_builds/{default_name}'
+
+    with cx.cd(target_dir):
+
+        print("testing the generated repo is okay")
+        print(f"cd {target_dir}")
+        cx.run("inv repo-test", echo=True)
+
 
--- repos/cookiecutter/tasks/config.py
+++ repos/cookiecutter/tasks/config.py
@@ -3,5 +3,3 @@
 # load the system configuration. You can override them in this module,
 # but beware it might break stuff
 from .sysconfig import *
-
-TEST_MESSAGE = "You can configure modules from this file"
