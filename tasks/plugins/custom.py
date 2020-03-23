f940d587738daf61b6ab9b9e5039afeae519ebe2
Samuel Lotz <samuel.lotz@salotz.info>
2020-03-23 16:08:34 -0400

    update requirements.txt files for repos

--- tasks/plugins/custom.py
+++ tasks/plugins/custom.py
@@ -3,4 +3,21 @@ some customizations in this file which is included by default."""
 
 from invoke import task
 
+import os.path as osp
+from pathlib import Path
 
+
+@task
+def pin_repo_deps(cx, repo='base'):
+    """Pins or upgrades pins of the specified repo's dependency specs."""
+
+    repo_path = Path("repos") / repo
+
+    assert osp.exists(repo_path), f"No repo named: {repo}"
+
+    req_in = repo_path / '.jubeo' / "requirements.in"
+    req_txt = repo_path / '.jubeo' / "requirements.txt"
+
+    assert osp.exists(req_in), "No 'requirements.in' file"
+
+    cx.run(f"pip-compile --upgrade --output-file={req_txt} {req_in}")
