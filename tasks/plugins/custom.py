25bcd70587282293530c2e0c4c1e62c5a390f17b
Samuel Lotz <samuel.lotz@salotz.info>
2020-04-14 11:25:22 -0400

    the update repo pins task now does all by default

--- tasks/plugins/custom.py
+++ tasks/plugins/custom.py
@@ -3,22 +3,36 @@ some customizations in this file which is included by default."""
 
 from invoke import task
 
+import os
 import os.path as osp
 from pathlib import Path
 
 
 @task
-def pin_repo_deps(cx, repo='base'):
-    """Pins or upgrades pins of the specified repo's dependency specs."""
+def pin_repo_deps(cx, repo=None):
+    """Pins or upgrades pins of the specified repo's dependency specs.
 
-    repo_path = Path("repos") / repo
+    If no repo is specified it updates all of them.
 
-    assert osp.exists(repo_path), f"No repo named: {repo}"
+    """
 
-    req_in = repo_path / '.jubeo' / "requirements.in"
-    req_txt = repo_path / '.jubeo' / "requirements.txt"
+    if repo is not None:
 
-    assert osp.exists(req_in), "No 'requirements.in' file"
+        repo_paths = [Path("repos") / repo]
 
-    cx.run(f"pip-compile --upgrade --output-file={req_txt} {req_in}")
+    else:
+
+        repo_paths = [Path("repos") / repo
+                      for repo in
+                      os.listdir("repos")]
+
+    for repo_path in repo_paths:
+        assert osp.exists(repo_path), f"No repo named: {repo}"
+
+        req_in = repo_path / '.jubeo' / "requirements.in"
+        req_txt = repo_path / '.jubeo' / "requirements.txt"
+
+        assert osp.exists(req_in), "No 'requirements.in' file"
+
+        cx.run(f"pip-compile --upgrade --output-file={req_txt} {req_in}")
 
